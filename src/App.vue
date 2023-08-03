25777d25e108efc878110ec58fba0fc9d8a4292f
David <2295458@vaniercollege.qc.ca>
2023-08-03 09:04:55 -0400

    first commit?

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,49 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  <CourseList
+    msg="List of Courses"
+    v-for="course in courses"
+    :key="course.name"
+    :course="course"
+  />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+import CourseList from "./components/Course-list.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    // HelloWorld,
+    CourseList,
+  },
+
+  data() {
+    return {
+      count: 0,
+      courses: [
+        {
+          name: "Math",
+          description: "Learn to add number",
+          id: 1,
+          student: 15,
+        },
+        { name: "Science", description: "What is this", id: 2, student: 10 },
+        {
+          name: "French",
+          description: "Can you say Bonjour",
+          id: 3,
+          student: 5,
+        },
+      ],
+    };
+  },
+  methods: {
+    addOne() {
+      this.count++;
+    },
   },
 };
 </script>
@@ -22,5 +56,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+div {
+  border: #2c3e50 2px solid;
 }
 </style>
---
+++ src/components/Course-list.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <p>Course : {{ course.name }}</p>
+    <p>Description : {{ course.description }}</p>
+    <p>Course ID : {{ course.id }}</p>
+    <p>Student #: {{ course.student + count }} / 20</p>
+    <button @click="adding">Add</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CourseList",
+  props: {
+    msg: String,
+    course: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    adding() {
+      this.count++;
+    },
+  },
+};
+</script>
